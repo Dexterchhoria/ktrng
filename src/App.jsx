e9a3753cc3a6bbad770ca179d9dbe9840e07fb63
Aakarsh Verma <ansh62050@gmail.com>
2025-04-02 02:29:55 +0530

    added lode and route fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
-import Home from "./pages/home/Home";
-import Gallery from "./pages/gallery/Gallery";
-import Events from "./pages/events/Events";
+import Loader from "./common/Loader.jsx"; // Create a separate loader component
+
+// Lazy load pages
+const Home = lazy(() => import("./pages/home/Home"));
+const Gallery = lazy(() => import("./pages/gallery/Gallery"));
+const Events = lazy(() => import("./pages/events/Events"));
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="home" index element={<Home />} />
-          <Route path="Gallery" element={<Gallery />} />
-          <Route path="events" element={<Events />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="" index element={<Home />} />
+            <Route path="home" index element={<Home />} />
+            <Route path="gallery" element={<Gallery />} />
+            <Route path="events" element={<Events />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
