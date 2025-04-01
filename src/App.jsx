aa41707daffd5b5051ce9d724f7c8120ed723e32
Siddharth Chhoria <116880199+Dexterchhoria@users.noreply.github.com>
2025-04-01 06:10:32 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="hero bg-black h-[100vh] w-[100vw] absolute">
+        <img className="relative h-[100%] w-[100%]" src="kaltarang.jpg" alt="" />
+
+      </div>
+    </>
+  )
+}
+
+export default App
